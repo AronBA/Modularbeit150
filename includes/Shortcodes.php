04869e377ec040bb8c815d558476b0a992949408
Klemen <klemenzmahar@gmail.com>
2023-01-18 19:16:43 +0100

    remade weatherShortcode styling

--- includes/Shortcodes.php
+++ includes/Shortcodes.php
@@ -6,7 +6,6 @@ class Shortcodes
     public function __construct()
     {
         $this->api = new WeatherApi("50e3d65a1f02700fcb69068f25d7521c");
-
     }
     function testShortcode(): string
     {
@@ -29,17 +28,19 @@ class Shortcodes
         $this->api->getData(12, 12);
         $cityName = $this->api->getCity();
         $country = $this->api->getCountry();
-        $temperature = $this->api->getTemperatur();
+        $temperature = $this->api->getTemperatur("c");
         $weatherDescription = $this->api->getWeatherDescription();
         $weatherIcon = $this->api->getWeatherIcon();
         $iconLink = "http://openweathermap.org/img/wn/$weatherIcon@4x.png";
-        return "<div style='border: solid black; border-radius: 15px; width: 500px; aspect-ratio: 1; display: flex; flex-direction: column; justify-content: space-between; align-items: center'>
+        return "<div style='height: 15rem;border: solid black; border-radius: 15px; width: 22rem; aspect-ratio: 1; display: flex; flex-direction: column; justify-content: space-between; align-items: center'>
                     <h2 style='border-radius: 10px 10px 0 0; margin: 0; height: 100px; background-color: #6ea7ff; width: 100%; display: flex; justify-content: center; align-items: center'>$cityName , $country</h2>
                     <div style='display: flex; align-items: center'>
-                        <img src='$iconLink' alt='weather icon'/>
-                        <h1>$temperature</h1>
+                    	<div style='flex-basis: 20%; display: flex; align-items: center; flex-direction: column;'>
+                    		<img src='$iconLink' alt='weather icon'/>
+                    		<h3 style='margin-top: -2rem'>$weatherDescription</h3>
+						</div>
+                        <h1 style='flex-basis: 50%'>$temperature °C</h1>
                     </div>
-                    <h3 style='border-top: solid #6ea7ff; width: 100%; text-align: center; margin: 0; display: grid; place-items: center; height: 80px;'>$weatherDescription</h3>
                 </div>";
     }
 }
