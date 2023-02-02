cfe619b2ac0d7c9a3f9d20846a0baa7e02b77f81
raphael-gi <raphael.gisler@stud.edubs.ch>
2023-02-02 17:08:18 +0100

    danger levels have descriptions

--- assets/scripts.js
+++ assets/scripts.js
@@ -34,9 +34,9 @@ function setProgress(time) {
 
 function setDangerLevels(speed) {
     const kmh = speed * 3.6
-    const levels = [{v:2,c:"66FFFF"},{v:5,c:"00FFFF"},{v:11,c:"00FF99"},{v:19,c:"00FF99"},
-        {v:29,c:"66FF66"},{v:39,c:"99FF33"},{v:50,c:"CCFF33"},{v:61,c:"FFFF00"},{v:74,c:"FFC000"},
-        {v:87,c:"FF9900"},{v:101,c:"FF6600"},{v:116,c:"FF3300"},{v:117,c:"FF0000"}]
+    const levels = [{v:2,c:"66FFFF",d:"Calm"},{v:5,c:"00FFFF",d:"Light air"},{v:11,c:"00FF99",d:"Light breeze"},{v:19,c:"00FF99",d:"Gentle breeze"},
+        {v:29,c:"66FF66",d:"Moderate breeze"},{v:39,c:"99FF33",d:"Fresh breeze"},{v:50,c:"CCFF33",d:"Strong breeze"},{v:61,c:"FFFF00",d:"Moderate gale"},{v:74,c:"FFC000",d:"Fresh gale"},
+        {v:87,c:"FF9900",d:"Strong gale"},{v:101,c:"FF6600",d:"Whole gale"},{v:116,c:"FF3300",d:"Violent storm"},{v:117,c:"FF0000",d:"Hurricane"}]
     let level = 0
     for (let i = levels.length-1; i > 0; i--) {
         if (kmh >= levels[i].v) {
@@ -48,11 +48,12 @@ function setDangerLevels(speed) {
     for (let i = 0; i < 12; i++) {
         const child = create()
         child.classList.add("dangerLevel")
-        if (i <= level) child.style.backgroundColor = "#" + levels[i].c
+        child.style.backgroundColor = "#" + levels[i].c
+        child.title = levels[i].d
+        if (i <= level) child.classList.add("active")
         dangerLevels.append(child)
     }
 }
-setDangerLevels(2)
 function setArrow(rotation) {
     const arrow = getId("arrow")
     arrow.style.setProperty("--arrowRotation", rotation + "deg")
